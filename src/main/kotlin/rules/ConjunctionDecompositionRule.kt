31551a6ac57823708897d9e8ad918926cc54f50e
Francieli Viane <francielifrv@gmail.com>
2024-12-01 15:23:33 +0100

    Introduce conjuntion decomposition rule

---
+++ src/main/kotlin/rules/ConjunctionDecompositionRule.kt
@@ -0,0 +1,29 @@
+package org.kr.assignment.rules
+
+import nl.vu.kai.dl4python.datatypes.Concept
+import nl.vu.kai.dl4python.datatypes.ConceptConjunction
+
+/**
+ * Equivalent to ⊓-rule 1:  If d has C ⊓ D assigned, assign also C and D to d
+ *
+ */
+class ConjunctionDecompositionRule : InferenceRule {
+
+    /**
+     * Includes all conjunctions related each [Concept] included in the [interpretation] to the [interpretation] itself.
+     * The rule is applied when at least one of the concepts included in the [interpretation]
+     * has type [ConceptConjunction]
+     *
+     * @param interpretation
+     * @return result with the updated interpretation if rule was applied. The original interpretation is returned if
+     * the rule is not applied.
+     */
+    override fun applyTo(interpretation: Set<Concept>): Result {
+        val conjuncts: List<Concept> = interpretation.filterIsInstance<ConceptConjunction>()
+            .flatMap { it.conjuncts }
+
+        return if (conjuncts.isNotEmpty()) {
+            return Result(RuleStatus.APPLIED, interpretation + conjuncts)
+        } else Result(RuleStatus.NOT_APPLIED, interpretation)
+    }
+}
---
+++ src/test/kotlin/rules/ConjunctionDecompositionRuleTest.kt
@@ -0,0 +1,68 @@
+package rules
+
+import nl.vu.kai.dl4python.datatypes.Concept
+import nl.vu.kai.dl4python.datatypes.ConceptConjunction
+import nl.vu.kai.dl4python.datatypes.ConceptName
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.kr.assignment.rules.ConjunctionDecompositionRule
+import org.kr.assignment.rules.RuleStatus
+import scala.collection.JavaConverters
+import scala.collection.Seq
+import java.util.*
+
+class ConjunctionDecompositionRuleTest {
+    private val rule = ConjunctionDecompositionRule()
+
+    @Test
+    fun `there are no changes in the interpretation when there are no conjunctions`() {
+        val interpretation = mutableSetOf<Concept>(
+            ConceptName("concept1"),
+            ConceptName("concept2"),
+        )
+
+        val result = rule.applyTo(interpretation)
+
+        assertThat(result.status).isEqualTo(RuleStatus.NOT_APPLIED)
+        assertThat(interpretation).hasSize(2)
+    }
+
+    @Test
+    fun `conjunctions related to a concept are added to the interpretation`() {
+        val conjunctions = createConjunctions(2)
+        val conceptConjunction = ConceptConjunction(conjunctions)
+
+        val interpretation = setOf<Concept>(
+            ConceptName("concept1"),
+            ConceptName("concept2"),
+            conceptConjunction
+        )
+
+        val result = rule.applyTo(interpretation)
+
+        assertThat(result.status).isEqualTo(RuleStatus.APPLIED)
+        assertThat(result.interpretation).hasSize(interpretation.size + conjunctions.size())
+        assertThat(result.interpretation).containsAnyOf(*conceptConjunction.conjuncts.toTypedArray())
+        assertThat(result.interpretation).containsAnyOf(*interpretation.toTypedArray())
+    }
+
+    @Test
+    fun `when all concepts are conjuctions, all related conjunctions are added to the interpretation`() {
+        val interpretation = setOf<Concept>(
+            ConceptConjunction(createConjunctions(1)),
+            ConceptConjunction(createConjunctions(3)),
+            ConceptConjunction(createConjunctions(5))
+        )
+
+        val result = rule.applyTo(interpretation)
+
+        assertThat(result.status).isEqualTo(RuleStatus.APPLIED)
+        assertThat(result.interpretation).hasSize(12)
+    }
+
+    private fun createConjunctions(numberOfConjunctions: Int): Seq<Concept> = List(numberOfConjunctions) {
+        ConceptName(UUID.randomUUID().toString())
+    }.let { conjunctions ->
+        JavaConverters.asScalaIterator<Concept>(conjunctions.iterator()).toSeq()
+    }
+}
